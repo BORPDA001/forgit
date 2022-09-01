628e68acf79eb9bca6b9837db79cd7af2b3baeee
BORPDA001 <xarmen836@gmail.com>
2022-09-01 19:36:14 +0400

    change html css js default file

--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
-let acc = document.getElementsByClassName("accordion");
-let i;
-let
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-// miacnel anjatel
-    this.classList.toggle("active");
-
-// miacnel anjatel arden claseri mijocov
-    let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
+alert("hello");
